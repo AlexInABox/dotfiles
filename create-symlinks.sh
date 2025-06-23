60870f8e87bba28db469d582671e2ee1c10c7136
alexinabox <main@alexinabox.de>
2025-06-23 17:15:46 +0200

    Added my mutt-temp

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -6,3 +6,7 @@ DOTFILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 for f in "$DOTFILES_DIR"/.*; do
   [[ -f "$f" && "$(basename "$f")" =~ ^\.[^.]+$ ]] && ln -sf "$f" ~/
 done
+
+# Link mutt-temp to /usr/local/share/mutt-wizard/mutt-temp
+sudo mkdir -p /usr/local/share/mutt-wizard
+sudo ln -sf "$DOTFILES_DIR/mutt-wizard/mutt-temp" /usr/local/share/mutt-wizard/mutt-temp
