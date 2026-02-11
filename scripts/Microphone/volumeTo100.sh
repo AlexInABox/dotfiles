859f0dda8503800b5f9adbd66b37459888b9fe17
AlexInABox <hobby@alexinabox.de>
2026-02-11 17:37:41 +0100

    now my source mic and the noice filter virtual mic are always at 100%

--- scripts/Microphone/volumeTo100.sh
+++ scripts/Microphone/volumeTo100.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while true; do
-    pactl set-source-volume "NoiseTorch Microphone for USB PnP Audio Device" 100%
+    pactl set-source-volume "rnnoise_source" 100%
+    pactl set-source-volume "alsa_input.usb-0c76_USB_PnP_Audio_Device-00.mono-fallback" 100%
     sleep 5
 done
