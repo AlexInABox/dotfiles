a04ca9b7aaa90dc81229f11656c1bfbff6eb3cef
AlexInABox <hobby@alexinabox.de>
2026-02-20 16:36:02 +0100

    Reduced mic sleep delay to 0

--- scripts/Microphone/volumeTo100.sh
+++ scripts/Microphone/volumeTo100.sh
@@ -2,5 +2,5 @@
 while true; do
     pactl set-source-volume "rnnoise_source" 100%
     pactl set-source-volume "alsa_input.usb-0c76_USB_PnP_Audio_Device-00.mono-fallback" 100%
-    sleep 5
+    sleep 0
 done
