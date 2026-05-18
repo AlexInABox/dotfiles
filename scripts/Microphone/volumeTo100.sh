fbc2d70472a85e2ea48b3541b3817945fd67d335
AlexInABox <hobby@alexinabox.de>
2026-05-18 17:28:30 +0200

    Update volumeTo100.sh to set easyeffects_source volume to 100%

--- scripts/Microphone/volumeTo100.sh
+++ scripts/Microphone/volumeTo100.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-    pactl set-source-volume "rnnoise_source" 100%
+    pactl set-source-volume "easyeffects_source" 100%
     pactl set-source-volume "alsa_input.usb-0c76_USB_PnP_Audio_Device-00.mono-fallback" 100%
     sleep 0
 done
